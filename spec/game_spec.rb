53174fe2d74be2fb4fd1e41c1cd4e74ee1ed0b09
Samuel Cox <samc1253@gmail.com>
2022-10-17 17:51:22 -0600

    Add cell and game classes with corresponding tests

---
+++ spec/game_spec.rb
@@ -0,0 +1,17 @@
+require "./lib/game"
+
+RSpec.describe Game do
+  describe "#initialize" do
+    it "exists" do
+      game = Game.new("board")
+
+      expect(game).to be_a(Game)
+    end
+
+    xit "has a board" do
+      game = Game.new
+
+      expect(game.board).to be_a(Board)
+    end
+  end
+end
