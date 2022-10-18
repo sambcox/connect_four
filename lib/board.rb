97eb6bd255966f63ceeb28ed3cec45fb0f42ca23
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-10-17 17:30:00 -0700

    Add board exists, columns, cells test

---
+++ lib/board.rb
@@ -0,0 +1,14 @@
+class Board
+  attr_reader :columns
+  def initialize
+    @columns = {
+      "A" => [Cell.new, Cell.new, Cell.new, Cell.new, Cell.new, Cell.new],
+      "B" => [Cell.new, Cell.new, Cell.new, Cell.new, Cell.new, Cell.new],
+      "C" => [Cell.new, Cell.new, Cell.new, Cell.new, Cell.new, Cell.new],
+      "D" => [Cell.new, Cell.new, Cell.new, Cell.new, Cell.new, Cell.new],
+      "E" => [Cell.new, Cell.new, Cell.new, Cell.new, Cell.new, Cell.new],
+      "F" => [Cell.new, Cell.new, Cell.new, Cell.new, Cell.new, Cell.new],
+      "G" => [Cell.new, Cell.new, Cell.new, Cell.new, Cell.new, Cell.new]
+    }
+  end
+end
---
+++ spec/board_spec.rb
@@ -0,0 +1,23 @@
+require './lib/board'
+require './lib/cell'
+
+RSpec.describe Board do
+  it 'exists' do
+    board = Board.new
+
+    expect(board).to be_a(Board)
+  end
+
+  it 'has columns' do
+    board = Board.new
+
+    expect(board.columns).to be_a(Hash)
+    expect(board.columns["A"]).to be_a(Array)
+  end
+
+  it 'columns have cells' do
+    board = Board.new
+
+    expect(board.columns["A"][0]).to be_a(Cell)
+  end
+end
