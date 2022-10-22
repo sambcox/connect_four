5d0f07dbad81d5486987f0b71e7a90131848e011
Samuel Cox <samc1253@gmail.com>
2022-10-21 18:04:18 -0600

    Add some functionality to game class with regards to diagonal wins and add tests for game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,8 +10,88 @@ RSpec.describe Game do
 
     it "has a board" do
       game = Game.new
+      game.test_start
 
       expect(game.board).to be_a(Board)
     end
+  end
+
+  describe '#win_game' do
+    it 'can win horizontally' do
+      game = Game.new
+      game.test_start
+      game.board.place_piece("A")
+      game.board.place_piece("B")
+      game.board.place_piece("C")
+      game.board.place_piece("D")
+
+      expect(game.win_game).to eq(true)
+    end
+
+    it 'can win vertically' do
+      game = Game.new
+      game.test_start
+      game.board.place_piece("A")
+      game.board.place_piece("A")
+      game.board.place_piece("A")
+      game.board.place_piece("A")
+
+      expect(game.win_game).to eq(true)
+
+      game = Game.new
+      game.test_start
+      game.board.place_piece("B")
+      game.board.place_piece("B")
+      game.board.place_piece("B")
+      game.board.place_piece("B")
+
+      expect(game.win_game).to eq(true)
+
+      game = Game.new
+      game.test_start
+      game.board.place_piece("C")
+      game.board.place_piece("C")
+      game.board.place_piece("C")
+      game.board.place_piece("C")
+
+      expect(game.win_game).to eq(true)
+
+      game = Game.new
+      game.test_start
+      game.board.place_piece("D")
+      game.board.place_piece("D")
+      game.board.place_piece("D")
+      game.board.place_piece("D")
+
+      expect(game.win_game).to eq(true)
+
+      game = Game.new
+      game.test_start
+      game.board.place_piece("E")
+      game.board.place_piece("E")
+      game.board.place_piece("E")
+      game.board.place_piece("E")
+
+      expect(game.win_game).to eq(true)
+
+      game = Game.new
+      game.test_start
+      game.board.place_piece("F")
+      game.board.place_piece("F")
+      game.board.place_piece("F")
+      game.board.place_piece("F")
+
+      expect(game.win_game).to eq(true)
+
+      game = Game.new
+      game.test_start
+      game.board.place_piece("G")
+      game.board.place_piece("G")
+      game.board.place_piece("G")
+      game.board.place_piece("G")
+
+      expect(game.win_game).to eq(true)
+    end
+    
   end
 end
